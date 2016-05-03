537578280ea6e4133999e8b6563dd5f770239c80
talrez <tal@way2cu.com>
2016-05-03 12:41:14 +0300

    Life at tgs: Configured mosaic object

--- site/scripts/mosaic_gallery.js
+++ site/scripts/mosaic_gallery.js
@@ -1,8 +1,8 @@
 $(window).load(function() {
 	$('.pictures').jMosaic({
 		items_type: "img", // Type of elements in the selector (Default: img);
-		min_row_height: 500, // Minimal row height (Default: 150);
-		margin: 3, // Space between elements (Default: 0);
-		is_first_big: true // First row - largest (Default: false);
+		min_row_height: 300, // Minimal row height (Default: 150);
+		margin: 1, // Space between elements (Default: 0);
+		is_first_big: false // First row - largest (Default: false);
 	});
 });
