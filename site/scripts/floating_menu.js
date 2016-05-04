f74c8c6b2af6ce73dd1a38b9067e12a0c3a3d558
talrez <tal@way2cu.com>
2016-05-04 09:27:27 +0300

    Floating menu: Removed object to seperate files and synced template pages

---
+++ site/scripts/floating_menu.js
@@ -0,0 +1,56 @@
+
+// create or use existing site scope
+var Site = Site || {};
+
+/**
+ * @param object menu               jQuery object
+ * @param object trigger_element    jQuery object
+ */
+function FloatingMenu(menu, trigger_element) {
+	var self = this;
+
+	self.menu = menu;
+	self.position = trigger_element.offset().top;
+	self.active = false;
+	  
+	/**
+	 * Object initialization.
+	 */
+	self._init = function() {
+		// connect signals
+		$(window).on('scroll', self.handle_scroll);
+
+		// set initial state
+		self.handle_scroll(null);
+	};
+	
+	/**
+	 * Handle window scroll.
+	 *
+	 * @param object event
+	 */
+	self.handle_scroll = function(event) {
+		var over_position = $(window).scrollTop() >= self.position;
+		
+		if (over_position && !self.active) {
+			self.menu.addClass('active');
+			self.active = true;
+
+		} else if (!over_position && self.active) {
+			self.menu.removeClass('active');
+			self.active = false;
+		}
+	};
+
+	// finalize object
+	self._init();
+}
+
+$(function() {
+	// create floating menu object
+	if($('div#intro').length > 0 || $('div.pictures').length > 0 ) {
+		Site.menu = new FloatingMenu($('header'),$('header'));
+	} else {
+		Site.menu = new FloatingMenu($('header'),$('section').first().next());
+	}
+})
--- site/scripts/main.js
+++ site/scripts/main.js
@@ -46,62 +46,11 @@ Site.is_mobile = function() {
 };
 
 /**
- * @param object menu               jQuery object
- * @param object trigger_element    jQuery object
- */
-function FloatingMenu(menu, trigger_element) {
-	var self = this;
-
-	self.menu = menu;
-	self.position = trigger_element.offset().top;
-	self.active = false;
-	  
-	/**
-	 * Object initialization.
-	 */
-	self._init = function() {
-		// connect signals
-		$(window).on('scroll', self.handle_scroll);
-
-		// set initial state
-		self.handle_scroll(null);
-	};
-	
-	/**
-	 * Handle window scroll.
-	 *
-	 * @param object event
-	 */
-	self.handle_scroll = function(event) {
-		var over_position = $(window).scrollTop() >= self.position;
-		
-		if (over_position && !self.active) {
-			self.menu.addClass('active');
-			self.active = true;
-
-		} else if (!over_position && self.active) {
-			self.menu.removeClass('active');
-			self.active = false;
-		}
-	};
-
-	// finalize object
-	self._init();
-}
-
-/**
  * Function called when document and images have been completely loaded.
  */
 Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
-
-	// create floating menu object
-	if($('div#intro').length > 0 || $('div.pictures').length > 0) {
-		Site.menu = new FloatingMenu($('header'),$('header'));
-	} else {
-		Site.menu = new FloatingMenu($('header'),$('section').first().next());
-	}
 
 	// function for filtering jobs
 	var filter_checkboxes = $('label.job input');
